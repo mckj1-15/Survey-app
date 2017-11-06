b704864a8afaf3871acc9539b542bc9fc2183416
mckj1-15 <32267202+mckj1-15@users.noreply.github.com>
2017-11-06 15:10:08 +0000

    storage

--- www/js/index.js
+++ www/js/index.js
@@ -8,8 +8,10 @@ $(document).on("pagecreate","#pageone",function(){
 function submitText() {
 	var text = $('#textinput').val();
 	alert(text);
+	storeValue("text", text)
 }
 
 function storeValue(key, value) {
 	//add some code to store the key-value pair in persistant storage 
+	window.localStorage.setItem(key, value);
 }
